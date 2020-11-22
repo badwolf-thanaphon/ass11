ee0e630b990b6e9d60f696648764370bb82ce752
badwolf-thanaphon <69790750+badwolf-thanaphon@users.noreply.github.com>
2020-11-22 23:41:27 +0700

    ส่งงานย้อนหลังlab 11

---
+++ app/src/main/java/com/id6130205356/ass11apk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.id6130205356.ass11apk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
